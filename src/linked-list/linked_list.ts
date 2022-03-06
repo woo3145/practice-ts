d2cc027bdaa41bce571e911891a7ed41beb78cbb
이창우 <ichang-u@ichang-us-MacBook-Air.local>
2022-03-05 16:39:20 -0800

    Linked List - 기능구현 (2)

--- src/linked-list/linked_list.ts
+++ src/linked-list/linked_list.ts
@@ -8,15 +8,15 @@ interface ILinkedList<T> {
   popFront(): T | null;
   pushBack(value: T): void;
   popBack(): T | null;
-  front(): T;
-  back(): T;
-  insert(index: number, value: T): T;
+  front(): T | null;
+  back(): T | null;
+  insert(index: number, value: T): void;
   erase(index: number): void;
   reverse(): void;
   removeValue(value: T): void;
 }
 
-class LinkedList<T> {
+class LinkedList<T> implements ILinkedList<T> {
   #head: Node<T> | null = null;
   #size: number = 0;
 
@@ -24,11 +24,25 @@ class LinkedList<T> {
     this.#head = node;
   }
 
+  getAll() {
+    let current = this.#head;
+    let str = "[ ";
+    while (current) {
+      str += current.getData();
+      if (current.getNext()) {
+        str += " -> ";
+      }
+      current = current.getNext();
+    }
+    str += " ]";
+    return str;
+  }
+
   size(): number {
     return this.#size;
   }
   empty(): boolean {
-    return this.#size === 0 ? false : true;
+    return this.#size === 0 ? true : false;
   }
   valueAt(index: number): T | null {
     let current = this.#head;
@@ -80,10 +94,9 @@ class LinkedList<T> {
   popBack(): T | null {
     let current = this.#head;
     let prev: Node<T> | null = null;
-    if (!current) {
-      return null;
+    if (current) {
+      this.#size -= 1;
     }
-    this.#size -= 1;
 
     while (current?.getNext()) {
       prev = current;
@@ -96,6 +109,91 @@ class LinkedList<T> {
     }
 
     return current ? current.getData() : null;
+  }
+
+  front(): T | null {
+    const current = this.#head;
+    if (!current) {
+      return null;
+    }
+    return current.getData();
+  }
+
+  back(): T | null {
+    let current = this.#head;
+
+    while (current?.getNext()) {
+      current = current.getNext();
+    }
+
+    return current ? current.getData() : null;
+  }
+
+  insert(index: number, value: T): void {}
+
+  erase(index: number) {
+    let current = this.#head;
+    let prev: Node<T> | null = null;
+    let count = 0;
+    if (!current) {
+      return;
+    }
+    // index번 다음데이터로 이동(없으면 그대로)
+    while (count < index) {
+      ++count;
+      if (current?.getNext()) {
+        prev = current;
+        current = current.getNext();
+      }
+    }
+    // 다음 데이터가 있으면 이전 노드나 헤드뒤에 index다음데이터 연결
+    if (current?.getNext()) {
+      if (prev) {
+        prev.setNext(current?.getNext());
+      } else {
+        this.#head = current.getNext();
+      }
+      // 다음 데이터가 없으면 마지막이므로 이전 노드의 다음데이터 제거
+    } else {
+      prev?.setNext(null);
+    }
+
+    this.#size -= 1;
+  }
+
+  reverse() {
+    let current = this.#head;
+    let temp: Node<T> | null = null;
+    let newHead: Node<T> | null = null;
+
+    // current로 모든배열 순회
+    while (current) {
+      // current값으로 새 노드를 생성하여 위에 만든 newHead를 꼬리로 계속 붙여주고
+      // 붙여진 값을 다시 newHead에 넣어 데이터를 head쪽으로 계속 추가시켜준다.
+      temp = new Node(current.getData());
+      current = current.getNext();
+      temp.setNext(newHead);
+      newHead = temp;
+    }
+    this.#head = newHead;
+  }
+
+  removeValue(value: T) {
+    let current = this.#head;
+    let prev: Node<T> | null = null;
+
+    while (current) {
+      if (current?.getData() === value) {
+        if (prev) {
+          prev.setNext(current.getNext());
+        } else {
+          this.setHead(current.getNext());
+        }
+        break;
+      }
+      prev = current;
+      current = current.getNext();
+    }
   }
 }
 
--- src/linked-list/main.ts
+++ src/linked-list/main.ts
@@ -4,34 +4,50 @@ const linkedListTest = () => {
   console.log("****** Linked List Test ******");
   const linkedList = new LinkedList<number>();
 
-  console.log("push front 1");
-  linkedList.pushFront(1);
-  console.log("push front 4");
-  linkedList.pushFront(4);
-  console.log("push front 9");
+  console.log("pushBack 1");
+  linkedList.pushBack(1);
+  console.log("pushBack 2");
+  linkedList.pushBack(2);
+  console.log("pushBack 3");
+  linkedList.pushBack(3);
+  console.log("pushBack 4");
+  linkedList.pushBack(4);
+  console.log("배열 확인", linkedList.getAll());
+  console.log("2번째 인덱스 수", linkedList.valueAt(2));
+  console.log("Empty?", linkedList.empty());
+
+  console.log("pushFront 9");
   linkedList.pushFront(9);
+  console.log("배열 확인", linkedList.getAll());
 
-  console.log("현재 사이즈 : ", linkedList.size());
-  console.log("0번째 수 : ", linkedList.valueAt(0));
-  console.log("1번째 수 : ", linkedList.valueAt(1));
-  console.log("pop front : ", linkedList.popFront());
-  console.log("현재 사이즈 : ", linkedList.size());
-  console.log("0번째 수 : ", linkedList.valueAt(0));
+  console.log("popFront : ", linkedList.popFront());
+  console.log("popBack : ", linkedList.popBack());
+  console.log("배열 확인", linkedList.getAll());
 
-  console.log("push back 0");
-  linkedList.pushBack(0);
+  console.log("제일 앞 : ", linkedList.front());
+  console.log("제일 뒤 : ", linkedList.back());
 
-  console.log("0번째 수 : ", linkedList.valueAt(0));
+  console.log("리버스");
+  linkedList.reverse();
+  console.log("배열 확인", linkedList.getAll());
 
-  console.log("1번째 수 : ", linkedList.valueAt(1));
+  console.log("1번째 인덱스 삭제");
+  linkedList.erase(1);
+  console.log("배열 확인", linkedList.getAll());
 
-  console.log("2번째 수 : ", linkedList.valueAt(2));
+  console.log("pushBack 1");
+  linkedList.pushBack(1);
+  console.log("pushBack 2");
+  linkedList.pushBack(2);
+  console.log("pushBack 3");
+  linkedList.pushBack(3);
+  console.log("pushBack 4");
+  linkedList.pushBack(4);
+  console.log("배열 확인", linkedList.getAll());
 
-  console.log("pop back", linkedList.popBack());
-  console.log("pop back", linkedList.popBack());
-  console.log("pop back", linkedList.popBack());
-  console.log("pop back", linkedList.popBack());
-  console.log("현재 사이즈 : ", linkedList.size());
+  console.log("처음 발견되는 3 삭제");
+  linkedList.removeValue(3);
+  console.log("배열 확인", linkedList.getAll());
 };
 
 export default linkedListTest;
