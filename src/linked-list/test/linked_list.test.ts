0d547d0e67dfe569fcb9cc7b24b6b8b1cd420ab2
Changwoo Lee <lcwoo31456@gmail.com>
2022-03-07 06:12:52 +0900

    Linked List 테스트 (1)

--- src/linked-list/test/linked_list.test.ts
+++ src/linked-list/test/linked_list.test.ts
@@ -1,3 +1,27 @@
-test("1 is 1", () => {
-  expect(1).toBe(1);
+import LinkedList from "../linked_list";
+
+const linkedList = new LinkedList<number>();
+
+describe("linked List Test", () => {
+  test("empty", () => {
+    expect(linkedList.empty()).toBe(true);
+  });
+  test("push Method", () => {
+    linkedList.pushBack(1);
+    linkedList.pushBack(2);
+    linkedList.pushBack(3);
+    linkedList.pushFront(4);
+    expect(linkedList.front()).toBe(4);
+    expect(linkedList.back()).toBe(3);
+    expect(linkedList.size()).toBe(4);
+    linkedList.insert(2, 9);
+    expect(linkedList.valueAt(2)).toBe(9);
+  });
+
+  test("pop Method", () => {
+    expect(linkedList.popFront()).toBe(4);
+    expect(linkedList.popBack()).toBe(3);
+    console.log(linkedList.getAll());
+    expect(linkedList.size()).toBe(3); // 에러발생
+  });
 });
